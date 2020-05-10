db61db5da825d164e85a2968fe2f0c6d65e49b6b
bear <thedarrendawson@gmail.com>
2020-05-10 03:27:11 -0700

    Add a ^C check to mixer_scraper_runner.py that allows all worker threads to finish their commits to the database cleanly before killing the program

--- mixer_scraper_runner.py
+++ mixer_scraper_runner.py
@@ -11,6 +11,7 @@
 import sys
 import json
 import time
+import signal
 import datetime
 import threading
 
@@ -20,17 +21,19 @@ from mixer_scraper import *
 
 # Hyper Parameters -------------------------------------------------------------
 
+# lookup table indicating how many 30 second sleep sessions a thread should take before starting work again
+__sleep_period = 30
 __sleep = {
-    'main_thread':        1 * 60 * 30,   # <- run every 30 minutes
-    'scrape_livestreams': 1 * 60 * 1,   # <- run every 15 minutes
-    'scrape_recordings':  1 * 60 * 1    # <- run every 10 minutes
+    'scrape-livestreams': 2 * 15,   # <- run every 15 minutes
+    'scrape-recordings':  2 * 5     # <- run every 10 minutes
 }
 
 
 # Threading Related Variabels --------------------------------------------------
 
-worker_threads = {}       # <- lookup table of {thread_id: thread}
+worker_threads   = {}     # <- lookup table of {thread_id: thread}
 thread_functions = {}     # <- lookup table of {thread_id: function to run for thread }
+thread_status    = {}     # <- lookup table of {thread_id: status}, if status == 'end', then the thread is flagged to terminate
 __thread_id_livestreams = 'Scrape Livestreams'
 __thread_id_recordings  = 'Scrape Recordings'
 
@@ -45,7 +48,10 @@ def thread_scrape_livestreams(thread_id):
         print_from_thread(thread_id, "starting work")
         mixer_scraper.procedure_scrape_livestreams()
         print_from_thread(thread_id, "sleeping")
-        time.sleep(__sleep['scrape_livestreams'])
+        for i in range(__sleep['scrape-livestreams']):
+            if (thread_status[thread_id] == 'end'):
+                return
+            time.sleep(__sleep_period)
 
 
 def thread_scrape_recordings(thread_id):
@@ -54,7 +60,10 @@ def thread_scrape_recordings(thread_id):
         print_from_thread(thread_id, "starting work")
         mixer_scraper.procedure_scrape_recordings()
         print_from_thread(thread_id, "sleeping")
-        time.sleep(__sleep['scrape_recordings'])
+        for i in range(__sleep['scrape-recordings']):
+            if (thread_status[thread_id] == 'end'):
+                return
+            time.sleep(__sleep_period)
 
 # ==============================================================================
 # Main
@@ -73,11 +82,35 @@ def create_worker_thread(thread_id):
 
     # run the thread
     worker_threads[thread_id] = threading.Thread(target=thread_functions[thread_id], args=(thread_id, ))
+    thread_status[thread_id]  = 'live'
     worker_threads[thread_id].start()
     return
 
 
 
+# function that allows all threads to terminate gracefully
+def stop_scraper(sig, frame):
+    print('\n-------------------------------------------------')
+    print("Shutting Down")
+    print('-------------------------------------------------')
+    print('Please wait for all threads to finish their commits')
+    print('This may take a while...\n')
+
+    # signal for threads to terminate
+    for thread_id in thread_status:
+        thread_status[thread_id] = 'end'
+
+    # wait for threads to join
+    for thread_id in worker_threads:
+        print_from_thread(thread_id, "waiting for thread to finish work...")
+        worker_threads[thread_id].join()
+        print_from_thread(thread_id, "terminated")
+
+    print('shut down complete!\n')
+    sys.exit(0)
+
+
+# main function
 def run():
 
     # initialize databases if need be
@@ -87,9 +120,9 @@ def run():
     # start threads on scraping procedures
     create_worker_thread(__thread_id_livestreams)
     create_worker_thread(__thread_id_recordings)
-    while(True):
-        time.sleep(__sleep['main_thread'])
 
+    signal.signal(signal.SIGINT, stop_scraper)
+    signal.pause()
 
 # Run --------------------------------------------------------------------------
 
